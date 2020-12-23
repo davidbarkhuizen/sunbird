ba538d2ba1e8453709f1511bca6acff44565651a
david barkhuizen <david.barkhuizen@gmail.com>
2020-12-23 20:30:09 +0200

    wip

--- rpi/server.sh
+++
@@ -1,4 +0,0 @@
-arduino --upload ../arduino/sunbird/sunbird.ino
-
-export FLASK_APP=server.py
-flask run  --port=8888 --host=0.0.0.0
---
+++ rpi/sunbird-server.sh
@@ -0,0 +1,1 @@
+python3 sunbird-server.py --host 0.0.0.0 --port 8888 --serial /dev/ttyUSB0 --baud 9600
