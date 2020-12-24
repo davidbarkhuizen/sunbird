39361666a2f462b1b6f0e1b6581ce8cd5e73185c
david barkhuizen <david.barkhuizen@gmail.com>
2020-12-24 08:05:22 +0200

    reorg

--- sungate/run.sh
+++ sungate/run.sh
@@ -1,2 +1,2 @@
-echo usage: run.sh <serial device> e.g. run.sh /dev/ttyACM0
-python3 sungate.py --host 0.0.0.0 --port 8888 --baud 9600 --serial "$1"
+# usage: run.sh <serial device> e.g. run.sh /dev/ttyACM0
+python3 rpi/sungate.py --host 0.0.0.0 --port 8888 --baud 9600 --serial "$1"
