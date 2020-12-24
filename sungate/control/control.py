4b1f683ef79157a95e94e55e8bb24c60d1efdd75
david barkhuizen <david.barkhuizen@gmail.com>
2020-12-24 14:44:15 +0200

    working on control - ignore

--- sungate/control/control.py
+++ sungate/control/control.py
@@ -1,3 +1,14 @@
+# Thrust
+# spin but not take off 
+# => T = 20
+# 
+# 
+# take off and hover
+# full battery => 80
+# low battery => 100
+# 
+# T = 150 => nothing ?
+
 import argparse
 
 parser = argparse.ArgumentParser(description='sungate control client')
@@ -24,12 +35,42 @@ def takeOffAndLandNoHover(url):
 
 def fwd(url):
     sendCommand(url, { 'throttle': 0, 'fwdback': 0, 'leftright': 0 })
-    time.sleep(1)
-    # sendCommand(url, { 'throttle': 80, 'fwdback': 0, 'leftright': 0 })
-    # time.sleep(1)
-    sendCommand(url, { 'throttle': 80, 'fwdback': 10, 'leftright': 0 })
-    time.sleep(3)
+    time.sleep(2)
+    sendCommand(url, { 'throttle': 20, 'fwdback': 0, 'leftright': 0 })
+    time.sleep(4)
+    sendCommand(url, { 'throttle': 100, 'fwdback': 0, 'leftright': 0 })
+    time.sleep(4)
+    sendCommand(url, { 'throttle': 100, 'fwdback': -20, 'leftright': 0 })
+    time.sleep(2)
+    sendCommand(url, { 'throttle': 100, 'fwdback': 0, 'leftright': 0 })
+    time.sleep(4)
     sendCommand(url, { 'throttle': 0, 'fwdback': 0, 'leftright': 0 })
 
+thover = 100 # full battery
+thover = 80 # empty battery
+tdescend = 50
 
-fwd(f'http://{host}:{port}')
+def leftright(url):
+    sendCommand(url, { 'throttle': 0, 'fwdback': 0, 'leftright': 0 })
+    time.sleep(2)
+    sendCommand(url, { 'throttle': 20, 'fwdback': 0, 'leftright': 0 })
+    time.sleep(4)
+    sendCommand(url, { 'throttle': thover, 'fwdback': 0, 'leftright': 0 })
+    time.sleep(3)
+    sendCommand(url, { 'throttle': thover, 'fwdback': 0, 'leftright': 20 })
+    time.sleep(2)
+    sendCommand(url, { 'throttle': thover, 'fwdback': 0, 'leftright': -20 })
+    time.sleep(2)
+    sendCommand(url, { 'throttle': tdescend, 'fwdback': 0, 'leftright': 0 })
+    time.sleep(2)
+    sendCommand(url, { 'throttle': 0, 'fwdback': 0, 'leftright': 0 })
+
+def test(url):
+
+    for i in 
+
+    sendCommand(url, { 'throttle': 0, 'fwdback': 0, 'leftright': 0 })
+    time.sleep(2)
+
+
+test(f'http://{host}:{port}')
